5a89c6c610b7a29693b1403927dd8952c90baadc
troystribling <troy.stribling@gmail.com>
2010-09-13 10:56:43 -0400

    added image processing before caching to improve performance.

--- Classes/Views/OverlayRMMapView.m
+++ Classes/Views/OverlayRMMapView.m
@@ -12,8 +12,12 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface OverlayRMMapView (PrivateAPI)
 
--(CGImageRef)maskTile:(UIImage*)img;
--(CGImageRef)createBitmap:(UIImage*)img;
+- (CGImageRef)maskTile:(UIImage*)img;
+- (CGContextRef)createContextFromImage:(UIImage*)img;
+- (CGImageRef)getImageFromContext:(CGContextRef)ctx usingByteOrder:(CGBitmapInfo)bitmapInfo;
+- (CGImageRef)createBitmapFromImage:(UIImage*)img;
+- (void)drawImageForTile:(UIImage*)img inContext:(CGContextRef)ctx;
+- (void)createImageForTile:(UIImage*)tile inContext:(CGContextRef)ctx;
 
 @end
 
@@ -37,26 +41,49 @@
 	return self; 
 } 
 
+//===================================================================================================================================
+#pragma mark RMMapViewDelegate
+
 //-----------------------------------------------------------------------------------------------------------------------------------
-- (void)drawTileLayer:(CALayer*)aLayer inContext:(CGContextRef)aContext {
+- (void)drawTileLayer:(CALayer*)aLayer inContext:(CGContextRef)ctx {
     UIImage* layerImage = [aLayer valueForKey:@"image"];
-    CGRect boundBox = CGContextGetClipBoundingBox(aContext);
-    CGContextTranslateCTM(aContext, 0.0, boundBox.size.height);
-    CGContextScaleCTM(aContext, 1.0, -1.0);
-    CGContextDrawImage(aContext, boundBox, self.mapTileBackgroundImage.CGImage);
-    CGContextSetBlendMode(aContext, kCGBlendModeOverlay);
-    CGContextDrawImage(aContext, boundBox, [self maskTile:layerImage]);
+    CGRect boundBox = CGContextGetClipBoundingBox(ctx);
+    CGContextScaleCTM(ctx, 1.0, -1.0);
+    CGContextTranslateCTM(ctx, 0.0, -boundBox.size.height);
+    CGContextDrawImage(ctx, boundBox, layerImage.CGImage);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
--(CGImageRef)maskTile:(UIImage*)img {
-    CGImageRef ref1 = [self createBitmap:img];
+- (UIImage*)willSaveTileImage:(UIImage*)tileImage {
+    CGContextRef ctx = [self createContextFromImage:tileImage];     
+    [self createImageForTile:tileImage inContext:ctx];
+    UIImage* newImage = [UIImage imageWithCGImage:[self getImageFromContext:ctx usingByteOrder:kCGBitmapByteOrder32Little]];
+    return newImage;
+}
+
+//===================================================================================================================================
+#pragma mark OverlayRMMapView PrivateAPI
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (CGImageRef)maskTile:(UIImage*)img {
+    CGImageRef bitMapRef = [self createBitmapFromImage:img];
     const float colorMasking[6] = {0xBA, 0xBA, 0xBA, 0xBA, 0xBA, 0xBA};
-    return CGImageCreateWithMaskingColors(ref1, colorMasking);
+    return CGImageCreateWithMaskingColors(bitMapRef, colorMasking);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
--(CGImageRef)createBitmap:(UIImage*)img {
+- (CGImageRef)createBitmapFromImage:(UIImage*)img {
+    CGContextRef bitmapCtx = [self createContextFromImage:img];     
+    CGImageRef imgRef = img.CGImage;
+    int imgWidth = CGImageGetWidth(imgRef);
+    int imgHeight = CGImageGetHeight(imgRef);
+    CGRect rect = CGRectMake(0.0, 0.0, imgWidth, imgHeight);    
+    CGContextDrawImage(bitmapCtx, rect, imgRef);
+    return [self getImageFromContext:bitmapCtx usingByteOrder:kCGBitmapByteOrder32Little];
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (CGContextRef)createContextFromImage:(UIImage*)img {
     CGImageRef imgRef = img.CGImage;
     int imgWidth = CGImageGetWidth(imgRef);
     int imgHeight = CGImageGetHeight(imgRef);
@@ -64,12 +91,28 @@
     void* bufferData = malloc(bytes);    
     CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
     CGContextRef bitmapCtx = CGBitmapContextCreate(bufferData, imgWidth, imgHeight, 8, imgWidth*4, colorSpaceRef, kCGImageAlphaPremultipliedFirst);     
-    CGRect rect = CGRectMake(0.0, 0.0, imgWidth, imgHeight);    
-    CGContextDrawImage(bitmapCtx, rect, imgRef); 
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, CGBitmapContextGetData(bitmapCtx), imgWidth*imgHeight*4, NULL);
-    CGImageRef bitmapImgRef = CGImageCreate(imgWidth, imgHeight, 8, 32, imgWidth*4, colorSpaceRef, kCGBitmapByteOrder32Little, provider , NULL, NO, kCGRenderingIntentDefault);
+    CFRelease(colorSpaceRef);
+    return bitmapCtx;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (CGImageRef)getImageFromContext:(CGContextRef)ctx usingByteOrder:(CGBitmapInfo)bitmapInfo {
+    int imgWidth = CGBitmapContextGetHeight(ctx);
+    int imgHeight = CGBitmapContextGetHeight(ctx);
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, CGBitmapContextGetData(ctx), imgWidth*imgHeight*4, NULL);
+    CGImageRef bitmapImgRef = CGImageCreate(imgWidth, imgHeight, 8, 32, imgWidth*4, colorSpaceRef, bitmapInfo, provider , NULL, NO, kCGRenderingIntentDefault);
     CFRelease(colorSpaceRef);
     return bitmapImgRef;
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+- (void)createImageForTile:(UIImage*)tile inContext:(CGContextRef)ctx {
+    CGRect boundBox = CGContextGetClipBoundingBox(ctx);
+    CGContextDrawImage(ctx, boundBox, self.mapTileBackgroundImage.CGImage);
+    CGContextSetBlendMode(ctx, kCGBlendModeOverlay);
+    CGContextDrawImage(ctx, boundBox, [self maskTile:tile]);
+}
+
 
 @end
