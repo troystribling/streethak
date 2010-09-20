284c430538fa0cd4665f288979a4dd29c98791e8
troystribling <troy.stribling@gmail.com>
2010-09-19 23:08:29 -0400

    cleaning up

---
+++ Classes/Views/Inventory/InventoryViewController.h
@@ -0,0 +1,16 @@
+//
+//  InventoryViewController.h
+//  streethak
+//
+//  Created by Troy Stribling on 9/19/10.
+//  Copyright 2010 planBresearch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InventoryViewController : UITableViewController {
+
+}
+
+@end
